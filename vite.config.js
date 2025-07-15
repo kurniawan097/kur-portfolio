03b0efb990cda7b2ca9c497ea5d5e57ea782780c
kurniawan097 <kurniawan.kurni097@gmail.com>
2025-07-15 16:13:06 +0700

    Update 2

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,21 @@
+{
+  /*import {
+    defineConfig
+  } from 'vite'
+  import react from '@vitejs/plugin-react'
+  import tailwindcss from '@tailwindcss/vite'
+
+  // https://vitejs.dev/config/
+  export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  })*/
+}
+
 import {
   defineConfig
 } from 'vite'
@@ -6,6 +24,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/kur-portfolio/', // ← tambahkan ini sesuai nama repo
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
